284d9fab1927833521d7be52c3fc68b6febe4be9
matifandy <matifandy@gmail.com>
2020-11-05 12:15:05 -0300

    body-footer-songrow-header

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  token:
-    "BQCP8sQAg3rZMkycLBf_kUVh4e9U6oysoN1h5lQZUaTgJNfT8CgIS4tNkG8xGPrATJO4zl3hCkEvnGt6SgDxqt6QIMkBJGhNUQLix0QqyrI9EQd-j-BR1A4peDorjHp7bx1etQAo58otNQdK0XCSYaB6zXFAcAGjnlSsEbrpBNt5tZsn",
+  // token:
+  // "BQCP8sQAg3rZMkycLBf_kUVh4e9U6oysoN1h5lQZUaTgJNfT8CgIS4tNkG8xGPrATJO4zl3hCkEvnGt6SgDxqt6QIMkBJGhNUQLix0QqyrI9EQd-j-BR1A4peDorjHp7bx1etQAo58otNQdK0XCSYaB6zXFAcAGjnlSsEbrpBNt5tZsn",
 };
 
 const reducer = (state, action) => {
@@ -26,6 +26,12 @@ const reducer = (state, action) => {
       return {
         ...state,
         playlists: action.playlists,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
       };
 
     default:
